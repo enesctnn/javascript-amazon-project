f050912bab2142be0f343ccd704e383658006684
enesctnn <enespcetin@gmail.com>
2023-09-29 15:59:43 +0300

    delete products from html clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,7 @@ import { cart, removeFromCart } from '../data/cart.js'
 import { products } from '../data/products.js'
 import { formatPrice } from './utils/money.js';
 
-cart.forEach((cartItem, index) => {
+cart.forEach(cartItem => {
   const { productId, quantity } = cartItem;
 
   let matchingItem;
@@ -20,7 +20,7 @@ cart.forEach((cartItem, index) => {
 
   document.querySelector('.js-order-summary')
     .innerHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${productId}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -53,7 +53,7 @@ cart.forEach((cartItem, index) => {
           Choose a delivery option:
         </div>
         <div class="delivery-option">
-          <input type="radio" checked class="delivery-option-input" name="delivery-option-${index}">
+          <input type="radio" checked class="delivery-option-input" name="delivery-option-${productId}">
           <div>
             <div class="delivery-option-date">
               Tuesday, June 21
@@ -64,7 +64,7 @@ cart.forEach((cartItem, index) => {
           </div>
         </div>
         <div class="delivery-option">
-          <input type="radio" class="delivery-option-input" name="delivery-option-${index}">
+          <input type="radio" class="delivery-option-input" name="delivery-option-${productId}">
           <div>
             <div class="delivery-option-date">
               Wednesday, June 15
@@ -75,7 +75,7 @@ cart.forEach((cartItem, index) => {
           </div>
         </div>
         <div class="delivery-option">
-          <input type="radio" class="delivery-option-input" name="delivery-option-${index}">
+          <input type="radio" class="delivery-option-input" name="delivery-option-${productId}">
           <div>
             <div class="delivery-option-date">
               Monday, June 13
@@ -95,5 +95,9 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const { productId } = link.dataset;
       removeFromCart(productId);
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     });
   });
