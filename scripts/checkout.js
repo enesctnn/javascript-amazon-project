948de759af14911497e061d6a43fe5e5a9bc0a59
enesctnn <enespcetin@gmail.com>
2023-09-29 15:38:09 +0300

    link delete button js works

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js'
+import { cart, removeFromCart } from '../data/cart.js'
 import { products } from '../data/products.js'
 import { formatPrice } from './utils/money.js';
 
@@ -42,7 +42,7 @@ cart.forEach((cartItem, index) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link js-delete-link link-primary" data-product-id="${productId}">
             Delete
           </span>
         </div>
@@ -89,3 +89,11 @@ cart.forEach((cartItem, index) => {
     </div>
   </div>`
 });
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const { productId } = link.dataset;
+      removeFromCart(productId);
+    });
+  });
